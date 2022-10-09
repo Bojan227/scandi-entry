76b965c85e5f31fabb3cd21a9e987bab60e08f2f
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-10-09 15:02:07 +0200

    Add Cart Page

--- src/components/CartProductCard.jsx
+++ src/components/CartProductCard.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import left from '../images/left-vector.png';
+import right from '../images/right-vector.png';
+import { CarouselSlider } from './CarouselSlider';
+import { AttributeCard } from './AttributeCard';
 
 export class CartProductCard extends React.Component {
+  state = {
+    currentImage: 0,
+  };
+
+  nextImage = () => {
+    this.setState(state => ({
+      ...state,
+      currentImage:
+        state.currentImage + 1 === this.props.item.gallery.length
+          ? 0
+          : state.currentImage + 1,
+    }));
+  };
+
+  previousImage = () => {
+    this.setState(state => ({
+      ...state,
+      currentImage:
+        state.currentImage === 0
+          ? this.props.item.gallery.length - 1
+          : state.currentImage - 1,
+    }));
+  };
+
   render() {
     return (
       <div className="cart-product-card">
         <section className="left-container">
-          <h3>{this.props.item.name}</h3>
+          <section>
+            <h3>{this.props.item.brand}</h3>
+            <h5>{this.props.item.name}</h5>
+          </section>
           <div className="prices">
             <h4>
               {
@@ -13,23 +44,53 @@ export class CartProductCard extends React.Component {
                   .symbol
               }
             </h4>
-            <h4>{this.props.item.prices[this.props.currentCurrency].amount}</h4>
+            <h4>
+              {(
+                this.props.item.prices[this.props.currentCurrency].amount *
+                this.props.item.quantity
+              ).toFixed(2)}
+            </h4>
           </div>
           <div className="attributes">
-            {console.log(this.props.item)}
-            <h2>{this.props.item.attributes[0].name}</h2>
-            {this.props.item.attributes[0].items.map((item, i) => {
-              return <button key={i}>{item.displayValue}</button>;
+            {this.props.item.attributes.map((item, i) => {
+              return (
+                <AttributeCard
+                  key={i}
+                  {...item}
+                  type={item.type}
+                  selectedAttributes={this.props.selectedAttributes}
+                  addAttribute={this.props.addAttribute}
+                />
+              );
             })}
           </div>
         </section>
         <section className="buttons">
-          <button>+</button>
-          <h4>{this.props.item.quantity}</h4>
-          <button></button>
+          <button onClick={this.props.incrementQuantity}>+</button>
+          <button>{this.props.item.quantity}</button>
+          <button onClick={this.props.decrementQuantity}>-</button>
         </section>
-        <section>
-          <img src="someimg" alt="" />
+        <section style={{ position: 'relative' }}>
+          <div>
+            {!this.props.cartIsOpen ? (
+              <img
+                style={{ width: '120px', height: '180px' }}
+                src={this.props.item.gallery[0]}
+                alt=""
+              />
+            ) : (
+              <CarouselSlider
+                currentImage={this.state.currentImage}
+                gallery={this.props.item.gallery}
+              />
+            )}
+          </div>
+          <div onClick={() => this.previousImage()} className="left-caret">
+            <img src={left} alt="left" />
+          </div>
+          <div onClick={() => this.nextImage()} className="right-caret">
+            <img src={right} alt="right" />
+          </div>
         </section>
       </div>
     );
